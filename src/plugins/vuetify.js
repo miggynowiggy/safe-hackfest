2893d5dc964e620a97751ba4becd6887fe190748
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-11-27 20:45:59 +0800

    added vuetify thene

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,20 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+	icons: {
+		iconfont: "fa" || "md",
+	},
+	theme: {
+		options: {
+			customProperties: true,
+		},
+		themes: {
+			light: {
+				primary: "#9A12B3",
+				secondary: "#FF727D",
+				accent: "#FCB400",
+				error: "#f05454",
+			},
+		},
+	},
 });
