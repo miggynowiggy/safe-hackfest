0a0c5e02d070e6bdac10295afb15814625940fbe
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-11-27 20:46:29 +0800

    removed boilerplate code

---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-main>
+    <div class="home">
+      <center>
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h1>{{ $store.getters['sample/get_greet'] }}</h1>
+      </center>
+    </div>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
