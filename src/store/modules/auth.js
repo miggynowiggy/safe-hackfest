491e2caaa6d641da47d32324483ce32934a89323
Kiel-Hyre <vinceadisip@gmail.com>
2020-11-28 12:19:08 +0800

    fix missing user

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -4,16 +4,16 @@ import clone from "lodash/cloneDeep";
 export default {
   namespaced: true,
   state: {
-    user: {},
+    user: {}
   },
   getters: {
-    GET_USER: (state) => state.user,
+    GET_USER: state => state.user
   },
   mutations: {
     SET_USER(state, payload) {
       state.user = clone(payload);
     },
-    DELETE_USER: state => state.user = null,
+    DELETE_USER: state => (state.user = null)
   },
   actions: {
     async SIGN_UP({ commit }, user) {
@@ -22,20 +22,31 @@ export default {
         let name;
         let type;
 
-        if(user.hasOwnProperty('company')) {
+        if (user.hasOwnProperty("company")) {
           name = user.company;
           type = "provider";
-
         } else {
           name = `${user.firstName} ${user.lastName}`;
           type = "individual";
         }
 
-        const createdUser = await AUTH.createUserWithEmailAndPassword(email, password);
-        await DB.collection("users").doc(createdUser.uid).set({ name, email, type });
-        commit("SET_USER", { name, email, type });
+        const createdUser = await AUTH.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+        const reference = {
+          name: name,
+          email: email,
+          type: type,
+          displayPhoto: "",
+          bookmarks: []
+        };
+        await DB.collection("users")
+          .doc(createdUser.user.uid)
+          .set(reference);
+        reference.id = createdUser.user.uid;
+        commit("SET_USER", reference);
         return true;
-
       } catch (error) {
         throw error;
       }
@@ -43,14 +54,15 @@ export default {
     async LOGIN({ commit }, { email, password }) {
       try {
         const user = await AUTH.signInWithEmailAndPassword(email, password);
-        const userRef = await DB.collection("users").doc(user.uid).get();
+        const userRef = await DB.collection("users")
+          .doc(user.user.uid)
+          .get();
         const userData = {
-          ... userRef.data(),
+          ...userRef.data(),
           id: userRef.id
         };
         commit("SET_USER", userData);
         return true;
-
       } catch (error) {
         throw error;
       }
@@ -61,7 +73,6 @@ export default {
         commit("DELETE_USER");
         dispatch("STOP_OBSERVERS");
         return true;
-
       } catch (error) {
         throw error;
       }
@@ -71,15 +82,16 @@ export default {
     },
     async RELOAD_USER({ commit }, uid) {
       try {
-        const user = await DB.collection("users").doc(uid).get();
+        const user = await DB.collection("users")
+          .doc(uid)
+          .get();
         let data = {
           ...user.data(),
           id: user.id
         };
         commit("SET_USER", data);
         return true;
-
-      } catch(error) {
+      } catch (error) {
         throw error;
       }
     },
@@ -87,31 +99,35 @@ export default {
       try {
         const provider = new FB.auth.GoogleAuthProvider();
         const result = await AUTH.signInWithPopup(provider);
-        const uid = result.user.uid
+        const uid = result.user.uid;
         let user = {
           name: result.user.displayName,
           email: result.user.email,
           displayPhoto: result.user.photoURL,
           type: null,
+          bookmarks: []
         };
 
-        const existingUser = await DB.collection('users').doc(uid).get();
-        if(!existingUser.exists) {
-          await DB.collection('users').doc(uid).set(user);
+        const existingUser = await DB.collection("users")
+          .doc(uid)
+          .get();
+        if (!existingUser.exists) {
+          await DB.collection("users")
+            .doc(uid)
+            .set(user);
           user.id = uid;
-          commit('SET_USER', user);
-
+          commit("SET_USER", user);
         } else {
           const data = {
             ...existingUser.data(),
             id: existingUser.id
-          }
-          commit('SET_USER', data);
+          };
+          commit("SET_USER", data);
         }
         return true;
-      } catch(error) {
+      } catch (error) {
         throw error;
       }
     }
-  },
+  }
 };
