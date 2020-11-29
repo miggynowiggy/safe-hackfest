c2701bec3e97426ab0d08ae6b0a1f63682f62e34
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-11-29 08:41:15 +0800

    added notice component for showing snackbar

---
+++ src/components/Notice.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-snackbar
+    v-model="snackbarState"
+    top
+    right
+    dark
+    rounded
+    :color="snackbarColor"
+    >{{ snackbarMessage }}</v-snackbar
+  >
+</template>
+<script>
+export default {
+  name: "Notice",
+  data () {
+    return {
+      snackbarMessage: null,
+      snackbarState: false,
+      snackbarColor: "success"
+    }
+  },
+  methods: {
+    open(state, message) {
+      this.snackbarColor = state.toLowerCase() === "success"
+        ? "success"
+        : "error";
+      this.snackbarMessage = message;
+      this.snackbarState = true;
+    }
+  }
+}
+</script>
